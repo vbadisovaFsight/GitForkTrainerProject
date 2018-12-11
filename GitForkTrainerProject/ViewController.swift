8ea373458226f616746d6971e60734d0e55fa268
Viktoriia Badisova <vbadisova@gmail.com>
2018-12-11 13:41:12 +0300

    initial

---
+++ GitForkTrainerProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitForkTrainerProject
+//
+//  Created by Виктория Бадисова on 11/12/2018.
+//  Copyright © 2018 Виктория Бадисова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
